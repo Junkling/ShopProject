a527060c4ce3421ecd0b71ea490bb20869e504c9
(Jun) <(ddoly0106@gmail.com)>
2023-07-24 20:43:19 +0900

    login 구현1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
     <Header/>
-    <Home/>
+    <RouterView/>
     <Footer/>
 </template>
 
 <script>
-import Home from "@/pages/Home";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import store from "@/scripts/store";
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+import { watch } from 'vue';
 
 export default {
   name: 'App',
   components: {
-    Home,
     Header,
     Footer
+  },
+  setup(){
+    const check = ()=>{
+      axios.get("/api/account/check").then((data)=>{
+        console.log(data);
+          store.commit("setAccount", data||0);
+      })
+    };
+    const route = useRoute();
+    watch(route, ()=>{
+      check();
+    })
   }
 }
 </script>
