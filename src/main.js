98019fc9e6406686738aeb471133e556cedeca66
(Jun) <(ddoly0106@gmail.com)>
2023-08-22 17:23:58 +0900

    코드 리팩토링

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue';
 import store from "@/scripts/store";
 import router from "@/scripts/router";
 import App from './App.vue';
-// import axios from 'axios';
-// axios.defaults.headers.post['auth'] =  store.state.account.auth;
-// axios.interceptors.request.use((config)=>
-// {
-//     return config;
-// })
+import axios from 'axios';
+    axios.interceptors.request.use((config)=>
+    {
+      config.headers["auth"] = "Bearer "+store.state.account.auth;
+      return config;
+    })
+// }
 
 createApp(App).use(store).use(router).mount('#app')
  
